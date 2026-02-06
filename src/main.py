568a9b3a86116f44bd3e057bc01fcc69946b0fdb
ArtemB <balabashinan@gmail.com>
2026-02-06 22:45:39 +0300

    add GraphQL libraries & router

--- src/main.py
+++ src/main.py
@@ -1,15 +1,25 @@
+import strawberry
+
 from fastapi import FastAPI, APIRouter
+from strawberry.fastapi import GraphQLRouter
 
 from src.api.rest.product.views import product_router
 from src.api.rest.user.views import user_router
 
 app = FastAPI()
+schema = strawberry.Schema()
 
+# API routers
 api_v1_router = APIRouter(prefix="/v1/api")
 api_v1_router.include_router(product_router)
 api_v1_router.include_router(user_router)
 
+# GraphQL routers
+graphql_app = GraphQLRouter(schema)
+
+
 app.include_router(api_v1_router)
+app.include_router(graphql_app, prefix="/v1/graphql")
 
 
 @app.get("/")
