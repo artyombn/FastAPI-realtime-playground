940464ed93073e9fc6a2dae8dcf8c368dc15b795
ArtemB <balabashinan@gmail.com>
2026-02-07 20:31:30 +0300

    update User graphql query & add all_users query

--- src/api/graphql/resolvers.py
+++ src/api/graphql/resolvers.py
@@ -1,18 +1,43 @@
 import strawberry
 
+from src.core.user.services import UserService
 from src.api.graphql.schemas import UserSchema
 
 
 @strawberry.type
 class Query:
 
-    @strawberry.field(description="Get a graphql query.")
+    @strawberry.field(description="Get User query.")
     def user(self, id: int) -> UserSchema:
-        users = {
-            1: {"id": 1, "username": "Anton", "email": "anton@example.com"},
-            2: {"id": 2, "username": "Olga", "email": "olga@example.com"},
-        }
-        user_data = users.get(id)
-        if not user_data:
+        user = UserService.get_by_id(id)
+        if not user:
             raise ValueError("User not found.")
-        return UserSchema(**user_data)
+        return UserSchema(
+            id=id,
+            username=user.username,
+            email=user.email,
+            is_admin=user.is_admin,
+            permissions=user.permissions,
+        )
+
+    @strawberry.field(description="Get all Users query.")
+    def all_users(self) -> list[UserSchema]:
+        users = [
+            UserSchema(
+                id=user[0],
+                username=user[1].username,
+                email=user[1].email,
+                is_admin=user[1].is_admin,
+                permissions=user[1].permissions,
+            )
+            for user in UserService.get_all()
+        ]
+        return users
+
+
+# @strawberry.type
+# class Mutation:
+#
+#     @strawberry.field(description="Create a new User query.")
+#     def create_user(self):
+#         pass
