733adcaa4df4d23ad09880d254a001f096097f1a
ArtemB <balabashinan@gmail.com>
2026-02-07 20:30:43 +0300

    update GraphQL User schema & add Token schema

--- src/api/graphql/schemas.py
+++ src/api/graphql/schemas.py
@@ -6,3 +6,11 @@ class UserSchema:
     id: int
     username: str
     email: str
+    is_admin: bool
+    permissions: list[str]
+
+
+@strawberry.type
+class TokenSchema:
+    access_token: str
+    refresh_token: str
