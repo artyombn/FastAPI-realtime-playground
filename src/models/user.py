fb0d95458037ca7b8c7aab911d3add7beffa43fe
ArtemB <balabashinan@gmail.com>
2026-02-10 18:26:08 +0300

    add User ORM model

---
+++ src/models/user.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
+
+from src.infrastructure.database import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    username = Column(String(100), nullable=False)
+    is_admin = Column(Boolean, default=False)
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
