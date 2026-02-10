fd4e2d1e97f469c7563da8f902a0b39d342104f2
ArtemB <balabashinan@gmail.com>
2026-02-10 18:25:51 +0300

    create DB engine using sqlalchemy

---
+++ src/infrastructure/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+from src.config.settings import settings
+
+DATABASE_URL = settings.get_db_url()
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
