36e6f8d8d27bc4cf065e40aec4fb233e5b49c9a7
ArtemB <balabashinan@gmail.com>
2026-02-12 15:12:44 +0300

    upd UserRepository: use classmethods with session instead of repository_factory

--- src/database/repositories/user.py
+++ src/database/repositories/user.py
@@ -6,35 +6,33 @@ from src.database.models.user import UserORM
 
 
 class UserRepository:
-    def __init__(self, session: Session):
-        self.session = session
-
-    def get_by_username(self, username: str) -> UserORM | None:
+    @classmethod
+    def get_by_username(cls, session: Session, username: str) -> UserORM | None:
         query = select(UserORM).filter_by(username=username)
-        user = self.session.execute(query)
+        user = session.execute(query)
         return user.scalar_one_or_none()
 
-    def get_by_id(self, user_id: int) -> UserORM | None:
+    @classmethod
+    def get_by_id(cls, session: Session, user_id: int) -> UserORM | None:
         query = select(UserORM).filter_by(id=user_id)
-        user = self.session.execute(query)
+        user = session.execute(query)
         return user.scalar_one_or_none()
 
-    def get_all(self) -> Sequence[UserORM]:
+    @classmethod
+    def get_all(cls, session: Session) -> Sequence[UserORM]:
         query = select(UserORM)
-        users = self.session.execute(query)
+        users = session.execute(query)
         return users.scalars().all()
 
-    def add(self, user_data: CreateUserSchema):
+    @classmethod
+    def add(cls, session: Session, user_data: CreateUserSchema):
         user = UserORM(
             username=user_data.username,
             email=user_data.email,
             password=user_data.password,
             is_admin=user_data.is_admin,
+            permissions=user_data.permissions,
         )
-        self.session.add(user)
-        self.session.flush()
+        session.add(user)
+        session.flush()
         return user
-
-
-def user_repository_factory(session: Session):
-    return UserRepository(session)
