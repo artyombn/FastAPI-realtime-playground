660677a9f8bf7e7af61df19cde336f7ff1d0d6bc
ArtemB <balabashinan@gmail.com>
2026-02-14 19:28:14 +0300

    update settings getter & add get_db for app.dependency_overrides

--- src/database/base.py
+++ src/database/base.py
@@ -1,9 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-from src.config.settings import settings
+from src.config.settings import get_settings
 
+settings = get_settings()
 DATABASE_URL = settings.get_db_url()
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
