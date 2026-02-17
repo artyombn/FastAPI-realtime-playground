92824bb6d0b30ac7e616437323a5cf03f9909294
ArtemB <balabashinan@gmail.com>
2026-02-17 20:31:03 +0300

    add DB manager

---
+++ src/database/db_helper.py
@@ -0,0 +1,37 @@
+from collections.abc import AsyncGenerator
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from src.config.settings import get_settings
+
+
+class DatabaseHelper:
+    """Менеджер подключения к бд."""
+
+    def __init__(self, db_url: str, echo: bool = False) -> None:
+        """Инициализирует асинхронный engine и создает фабрику сессий."""
+
+        self.engine: AsyncEngine = create_async_engine(url=db_url, echo=echo)
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            expire_on_commit=False,
+            autocommit=False,
+            autoflush=False,
+        )
+
+    async def get_session(self) -> AsyncGenerator[AsyncSession, None]:
+        """Получение асинхронной сессии."""
+        async with self.session_factory() as session:
+            yield session
+
+    async def dispose(self) -> None:
+        """Утилизация Engine при завершении работы приложения."""
+        await self.engine.dispose()
+
+
+settings = get_settings()
+db_helper = DatabaseHelper(db_url=settings.get_db_url(), echo=False)
