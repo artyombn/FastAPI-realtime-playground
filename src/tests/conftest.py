00216975501df692bb5574e3ecd2aa52be2586bc
ArtemB <balabashinan@gmail.com>
2026-02-17 20:48:33 +0300

    add pytest_asyncio fixtures (engine, db_session, client) & use loop_scope to work inside the same session

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,33 +1,56 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator, Any
 import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import pytest_asyncio
+from httpx import AsyncClient, ASGITransport
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+)
 
-from src.config.settings import get_settings
-from src.main import app
-from src.database.base import Base, get_db
-
-settings = get_settings()
-DATABASE_URL = settings.get_db_url()
-engine = create_engine(DATABASE_URL)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-@pytest.fixture(scope="module", autouse=True)
-def setup_database():
-    Base.metadata.create_all(bind=engine)
-    yield
-    Base.metadata.drop_all(bind=engine)
+from src.core.user.exceptions import UserAlreadyExistsError
+from src.database.uow import UnitOfWork
+from src.main import app as main_app
+from src.database.base import Base
+from src.database.db_helper import db_helper
 
 
-@pytest.fixture(scope="function")
-def db_session():
-    session = TestingSessionLocal()
-    try:
+@pytest.fixture(scope="session")
+def app_database_url() -> str:
+    from src.config.settings import get_settings
+
+    settings = get_settings()
+    return settings.get_db_url()
+
+
+@pytest_asyncio.fixture(scope="session", loop_scope="session")
+async def engine(app_database_url) -> AsyncGenerator[AsyncEngine, None]:
+    """Создаёт тестовый engine один раз для всех тестов"""
+    engine = create_async_engine(app_database_url, echo=False)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield engine
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+    await engine.dispose()
+
+
+@pytest_asyncio.fixture(scope="function", loop_scope="session")
+async def db_session(engine) -> AsyncGenerator[AsyncSession, None]:
+    factory = async_sessionmaker(
+        bind=engine,
+        expire_on_commit=False,
+        autocommit=False,
+        autoflush=False,
+    )
+    async with factory() as session:
         yield session
-    finally:
-        session.rollback()
-        session.close()
+        await session.rollback()
 
 
 @pytest.fixture(scope="module")
@@ -39,7 +62,25 @@ def client():
         finally:
             db.close()
 
-    app.dependency_overrides[get_db] = override_get_db
-    client = TestClient(app)
-    yield client
-    app.dependency_overrides.clear()
+
+@pytest.fixture
+def session_override(db_session: AsyncSession):
+    async def get_session_override() -> AsyncGenerator[AsyncSession, None]:
+        yield db_session
+
+    main_app.dependency_overrides[db_helper.get_session] = get_session_override
+    yield
+    main_app.dependency_overrides.clear()
+
+
+@pytest_asyncio.fixture(scope="function", loop_scope="session")
+async def client(session_override) -> AsyncGenerator[AsyncClient, None]:
+    """
+    HTTP клиент для интеграционных тестов.
+    """
+    transport = ASGITransport(app=main_app)
+    async with AsyncClient(
+        transport=transport,
+        base_url="http://test",
+    ) as ac:
+        yield ac
