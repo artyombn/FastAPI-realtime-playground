a0f697cddea7231d55cc4c8ebe4a5610be84cc2d
ArtemB <balabashinan@gmail.com>
2026-02-18 18:47:19 +0300

    add test User login

--- src/tests/integration/test_users.py
+++ src/tests/integration/test_users.py
@@ -1,11 +1,16 @@
+import logging
 from http import HTTPStatus
 
+import bcrypt
 import pytest
 from httpx import AsyncClient
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.models.user import UserORM
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
 
 @pytest.mark.asyncio
@@ -68,3 +73,37 @@ async def test_register_user_already_exists(
 
     assert response.status_code == HTTPStatus.CONFLICT
     assert response.json() == {"detail": "User already exists"}
+
+
+@pytest.mark.asyncio
+async def test_user_login(client: AsyncClient, db_session: AsyncSession):
+    password = "MyCat@Barsik7"
+
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode(
+        "utf-8"
+    )
+
+    db_session.add(
+        UserORM(
+            username="test_user_login",
+            email="test_user_login@gmail.com",
+            password=hashed_password,
+        )
+    )
+    await db_session.commit()
+
+    stmt = select(UserORM).where(UserORM.username == "test_user_login")
+    result = await db_session.execute(stmt)
+    found = result.scalar_one_or_none()
+
+    log.debug(f"USER_FOUND = {found.__dict__}")
+
+    response = await client.post(
+        "/v1/api/users/login",
+        json={
+            "username": "test_user_login",
+            "password": "MyCat@Barsik7",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
