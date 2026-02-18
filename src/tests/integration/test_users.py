cb8ba1bf4e6873d2572332fc0862459c5b908530
ArtemB <balabashinan@gmail.com>
2026-02-18 18:07:45 +0300

    add integration test: user_alrady_exists

--- src/tests/integration/test_users.py
+++ src/tests/integration/test_users.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 import pytest
 from httpx import AsyncClient
 from sqlalchemy import select
@@ -17,7 +19,7 @@ async def test_register_user(client: AsyncClient, db_session: AsyncSession):
         },
     )
 
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
 
     body = response.json()
     assert body["username"] == "test_user2"
@@ -32,3 +34,37 @@ async def test_register_user(client: AsyncClient, db_session: AsyncSession):
     assert body["id"] == found.id
     assert found.username == "test_user2"
     assert found.email == "test_user2@gmail.com"
+
+
+@pytest.mark.asyncio
+async def test_register_user_already_exists(
+    client: AsyncClient, db_session: AsyncSession
+):
+    db_session.add(
+        UserORM(
+            username="test_user3",
+            email="test_user3@gmail.com",
+            password="MyCat@Barsik7",
+        )
+    )
+    await db_session.commit()
+
+    stmt = select(UserORM).where(UserORM.email == "test_user3@gmail.com")
+    result = await db_session.execute(stmt)
+    found = result.scalar_one_or_none()
+
+    assert found is not None
+    assert found.username == "test_user3"
+    assert found.email == "test_user3@gmail.com"
+
+    response = await client.post(
+        "/v1/api/users/register",
+        json={
+            "username": "test_user3",
+            "email": "test_user3@gmail.com",
+            "password": "MyCat@Barsik7",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {"detail": "User already exists"}
