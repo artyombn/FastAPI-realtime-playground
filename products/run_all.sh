2a9c336cbf063c640f85073103118f7fd42cc0a1
ArtemB <balabashinan@gmail.com>
2026-02-25 19:32:00 +0300

    MSA Products: delete & move unnecessary files

--- products/run_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#chmod +x run_all.sh
-#./run_all.sh
-
-echo "Starting App..."
-uvicorn src.main:app --reload
--- products/src/config/paths.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-SRC_DIR = BASE_DIR / "src"
-MEDIA_DIR: Path = SRC_DIR / "media"
-MEDIA_DIR.mkdir(parents=True, exist_ok=True)
